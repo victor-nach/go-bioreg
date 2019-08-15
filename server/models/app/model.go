0d6e9bacfac14aca784decefa1a8860458e97d90
victor-nach <viheanaco@gmail.com>
2019-08-15 20:31:14 +0100

    ft : users can sign up - create models and controllers for signing up - create models and controllers for get all users - connect to the database using gorm [delivers #2]

--- server/models/app/model.go
+++ server/models/app/model.go
@@ -1,12 +1,26 @@
 package app
 
 import (
-	
+	"go-bioreg/server/models/db"
 )
 
 // StatusResponse - standard status response
 type StatusResponse struct {
 	Status int `json:"status,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+// UserResponse - standard response for users
+type UserResponse struct {
+	Status 			int 		`json:"status,omitempty"`
+	Message 		string 		`json:"message,omitempty"`
+	Data 			db.User		`json:"data,omitempty"`
+}
+
+// AllUsersResponse - Standard user response for all users
+type AllUsersResponse struct {
+	Status 			int 		`json:"status,omitempty"`
+	Message 		string 		`json:"message,omitempty"`
+	Data 			[]db.User		`json:"data,omitempty"`
 }
 
---
+++ server/models/db/model.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	"time"
+)
+
+// User - defines standard user struct
+type User struct {
+	ID 						int			`json:"id,omitempty"`
+	Name 					string		`json:"name,omitempty"`
+	Address 				string		`json:"address,omitempty"`
+	Email 					string		`json:"email,omitempty"`
+	FingerPrint 			string		`json:"fingerPrint,omitempty"`
+	FingerPrintToSend 		string		`json:"fingerPrintToSend,omitempty"`
+	FingerPosition 			string		`json:"fingerPosition,omitempty"`
+	Gender 					string		`json:"gender,omitempty"`
+	DateCreated 			time.Time	`json:"dateCreated,omitempty"`
+	DateMoved 				time.Time	`json:"dateMoved,omitempty"`
+}
